2f17370ed0ee5503e08318b6e7689abedccc549b
Rian <rian.sj@gmail.com>
2019-12-27 13:41:33 +0700

    modify hero image

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,10 +4,12 @@ import '../styles/home.css'
 class Home extends Component {
   render() {
     return (
-      <div className="hero-image">
-        <div className="hero-content">
-          <div className="hero-title">Movies Catalogue</div>
-          <div className="hero-desc">Your Movies and TV Shows Info</div>
+      <div className="hero-container">
+        <div className="hero-image">
+          <div className="hero-content">
+            <div className="hero-title">Movies Catalogue</div>
+            <div className="hero-desc">Your Movies and TV Shows Info</div>
+          </div>
         </div>
       </div>
     )
