1aec9954f9705dffdf16a4baa83bffe73c7a0f19
Rian <rian.sj@gmail.com>
2020-03-06 09:03:39 +0700

    fix Navigation.js change addEventListener to removeEventListener

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -21,7 +21,7 @@ class Navigation extends React.Component {
   }
 
   componentWillUnmount() {
-    window.addEventListener('scroll', this.handleScroll)
+    window.removeEventListener('scroll', this.handleScroll)
   }
 
   handleScroll = (e) => {
