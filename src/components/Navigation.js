2e89f22c686be97923606840d5b129918b5d6b45
Rian <rian.sj@gmail.com>
2020-03-27 13:36:52 +0700

    add true in window.removeEventListener to prevent memory leadk in component Navigation, componentWillUnmount

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -21,7 +21,7 @@ class Navigation extends React.Component {
   }
 
   componentWillUnmount() {
-    window.removeEventListener('scroll', this.handleScroll)
+    window.removeEventListener('scroll', this.handleScroll, true)
   }
 
   handleScroll = (e) => {
